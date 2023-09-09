a95d904b26c6ae5a4df1e3fbf027f7af22dc25b1
@zlavrov <105747377+zlavrov@users.noreply.github.com>
2023-09-09 23:29:24 +0300

    Update script.js
    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,13 @@ tg.MainButton.textColor = "#F55353";
 tg.MainButton.color = "#143F6B";
 tg.MainButton.setParams({"color": "#143F6B"});
 
-$('#savedata').on('click', function() {
+
+let btn = document.getElementById("savedata");
+btn.addEventListener('click', function(){
+
  tg.MainButton.show();
 });
+
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
    tg.sendData('cherrys'); 
