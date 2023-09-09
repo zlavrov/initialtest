86ed70e4ac54d33e556cbb21e24ad4adb3824fbb
@zlavrov <105747377+zlavrov@users.noreply.github.com>
2023-09-09 19:05:47 +0300

    Update script.js
    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,5 @@
 let tg = window.Telegram.WebApp;
-let nameid = '';
-let contentid = '';
 
 $('#usercard').append(`<p>${tg.initDataUnsafe.user.username}</p><p>${tg.initDataUnsafe.user.id}</p>`);
 
-$('#savedata').on('click', function() {
-
-    nameid = $('#nameid').val();
-    contentid = $('#contentid').val();
-});
-
-Telegram.WebApp.onEvent('mainButtonClicked', function() {
-
-    let data = {
-        name: nameid,
-        content: contentid
-    }
-
-    tg.sendData(JSON.stringify(data));
-});
+tg.sendData('apple');
